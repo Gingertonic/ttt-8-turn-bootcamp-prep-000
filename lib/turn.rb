11c2aa0f51287c99860862b9cd1d9f2cd61c4249
Beth Schofield <bethmschofield@gmail.com>
2018-02-03 07:43:41 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,6 +6,9 @@ def display_board(board)
   puts " board[6] | board[7] | board[8] "
 end
 
+def input_to_index(input)
+  input.to_int
+
 def position_taken?(board, index)
   if board[index] == "X" || board[index] == "Y"
     return true
