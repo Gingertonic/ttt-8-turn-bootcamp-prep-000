db3ccb19b189edb9ac6eae00ccb758b32fbcd8ee
Beth Schofield <bethmschofield@gmail.com>
2018-02-03 07:39:43 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,6 +1,22 @@
-def display_board
+def display_board(board)
   puts " board[0] | board[1] | board[2] "
   puts "-----------"
   puts " board[3] | board[4] | board[5] "
   puts "-----------"
   puts " board[6] | board[7] | board[8] "
+end
+
+def position_taken?(board, index)
+  if board[index] == "X" || board[index] == "Y"
+    return true
+  else
+    return false
+  end
+end
+
+def valid_move?(board, index)
+  if position_taken?(board, index) == false && index.between?(0,8)
+    return true
+  end
+end
+
